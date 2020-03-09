758a25f5ade2f24d71e9c4738cd63df218db11e1
jason_storey <jasonstoreyireland@gmail.com>
2020-03-09 12:54:16 +0000

    Adds Raycast Extensions

---
+++ Runtime/Extensions/RaycastExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace JasonStorey 
+{
+    public static class RaycastExtensions
+    {
+        public static Ray ToRay(this Transform t) => new Ray(t.position,t.forward);
+        //public static RayProvider ToRay(this Transform t) => new TransformLookRay(t);
+    }
+}
